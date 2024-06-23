b563e5e3e663c0554119396b02bc4fb2a801527e
Liam Swain <liamswain4@gmail.com>
2024-06-23 14:49:12 +0100

    Made some improvements to corne layout

--- keyboards/crkbd/keymaps/terrorbyte/keymap.c
+++ keyboards/crkbd/keymaps/terrorbyte/keymap.c
@@ -42,37 +42,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYMBOL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,----------------------------------------------------------.
-      KC_TNRS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                        KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_GRAVE,
+      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                        KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_GRAVE,
   //|--------+--------+--------+--------+--------+--------|                    |----------+---------+--------+--------+--------+----------|
-      KC_TNRS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_NUBS, KC_MINUS,  KC_EQL, KC_LBRC, KC_RBRC, S(KC_NUBS),
+      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_NUBS, KC_MINUS,  KC_EQL, KC_LBRC, KC_RBRC, S(KC_NUBS),
   //|--------+--------+--------+--------+--------+--------|                    |----------+---------+--------+--------+--------+----------|
-      KC_TNRS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                        XXXXXXX,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,   KC_TRNS,
+      _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                        XXXXXXX,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,   _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+----------+---------+--------+--------+--------+----------|
-                                          KC_TNRS, KC_TNRS, KC_TNRS,   KC_TNRS,   KC_TNRS, KC_TNRS
+                                          _______, _______, _______,   _______,   _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
     [_NAVIGATION] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,------------------------------------------------------------------.
-      KC_TNRS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_PGUP, KC_HOME,   KC_UP,   KC_END,             XXXXXXX, XXXXXXX,
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_PGUP, KC_HOME,   KC_UP,   KC_END,             XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+---------+--------------------+--------|
        KC_DEL,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MEDIA_PLAY_PAUSE, KC_CAPS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+---------+--------------------+--------|
-      KC_TNRS, KC_LCTL,   KC_NO,   KC_NO,  KC_F11,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,             XXXXXXX, KC_TNRS,
+      _______, KC_LCTL,   KC_NO,   KC_NO,  KC_F11,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,             XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+---------+--------------------+--------|
-                                          KC_TNRS, KC_TNRS, KC_TNRS,   KC_TNRS,   KC_TNRS, KC_TNRS
+                                          _______, _______, _______,   _______,   _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
     [_MOUSE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          XXXXXXX, _______, XXXXXXX,    XXXXXXX, _______, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _SYMBOL, _NAVIGATION, _MOUSE);
+}
